939cf7f6c8d11001d9659bfe11a06548561756d5
Damien Wright <damien@Damiens-MacBook-Pro.local>
2016-11-11 23:12:51 -0800

    Created Main and Saved Components; started working on Routes

---
+++ app/components/main.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Main = React.createClass({
+	render: function(){
+
+		return(
+			<div>Navigation</div>
+		)
+	}
+});
+
+module.exports = Main;
---
+++ app/components/saved.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Saved = React.createClass({
+	render: function(){
+
+		return(
+			<div>Saved Articles</div>
+		)
+	}
+});
+
+module.exports = Saved;
